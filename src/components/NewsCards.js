00a0dcb637e3950c6acbe5c2decd2f438c88da6b
Tisha Saha <tishasaha32@gmail.com>
2024-05-05 16:19:10 +0530

    setting up json server and adding all the data

---
+++ src/components/NewsCards.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+import useNews from "../hooks/useNews";
+
+function NewsCards() {
+  //   const apiUrl = "https://fakenews.squirro.com/news/sport";
+  //   useEffect(() => {});
+  //   const { articles } = useNews(apiUrl);
+  //   console.log(articles);
+  return <div>NewsCards</div>;
+}
+
+export default NewsCards;
