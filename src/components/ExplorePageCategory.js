8a2c410c7db58093c57569ddd3386d3d6a890bbc
Tisha Saha <tishasaha32@gmail.com>
2024-05-06 21:08:04 +0530

    Explore page categories code modified

--- src/components/ExplorePageCategory.js
+++ src/components/ExplorePageCategory.js
@@ -6,35 +6,41 @@ import technology from "../assets/category/technology.jpg";
 import styles from "./ExplorePageCategory.module.css";
 
 function ExplorePageCategory() {
+  const categories = [
+    {
+      id: 1,
+      name: "Business",
+      image: business,
+    },
+    {
+      id: 2,
+      name: "Entertainment",
+      image: entertainment,
+    },
+    {
+      id: 3,
+      name: "Sports",
+      image: sports,
+    },
+    {
+      id: 4,
+      name: "Technology",
+      image: technology,
+    },
+  ];
   return (
-    <div className={styles.categoriesContainer}>
-      <div className={styles.categoryContainer}>
-        <img src={business} className={styles.categoryImg} />
-        <div className={styles.categoryAndExploreButton}>
-          <p className={styles.category}>Business</p>
-          <button className={styles.exploreButton}>Explore</button>
-        </div>
-      </div>
-      <div className={styles.categoryContainer}>
-        <img src={entertainment} className={styles.categoryImg} />
-        <div className={styles.categoryAndExploreButton}>
-          <p className={styles.category}>Entertainment</p>
-          <button className={styles.exploreButton}>Explore</button>
-        </div>
-      </div>
-      <div className={styles.categoryContainer}>
-        <img src={sports} className={styles.categoryImg} />
-        <div className={styles.categoryAndExploreButton}>
-          <p className={styles.category}>Sports</p>
-          <button className={styles.exploreButton}>Explore</button>
-        </div>
-      </div>
-      <div className={styles.categoryContainer}>
-        <img src={technology} className={styles.categoryImg} />
-        <div className={styles.categoryAndExploreButton}>
-          <p className={styles.category}>Technology</p>
-          <button className={styles.exploreButton}>Explore</button>
-        </div>
+    <div>
+      <h3 className={styles.header}>Explore by Category</h3>
+      <div className={styles.categoriesContainer}>
+        {categories.map((category) => (
+          <div className={styles.categoryContainer} key={category.id}>
+            <img src={category.image} className={styles.categoryImg} />
+            <div className={styles.categoryAndExploreButton}>
+              <p className={styles.category}>{category.name}</p>
+              <button className={styles.exploreButton}>Explore</button>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,14 +1,12 @@
 import React from "react";
 import BottomNavbar from "../common/BottomNavbar";
 import Header from "../common/Header";
-import styles from "./Explore.module.css";
 import ExplorePageCategory from "../components/ExplorePageCategory";
 
 function Explore() {
   return (
     <div>
       <Header />
-      <h3 className={styles.header}>Explore by Category</h3>
       <ExplorePageCategory />
       <BottomNavbar activePage="Explore" />
     </div>
