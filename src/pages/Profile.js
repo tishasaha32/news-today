76c8d0372d0db97dfa638c9b3c42c672866f81c1
Tisha Saha <tishasaha32@gmail.com>
2024-05-27 22:52:36 +0530

    Designed the user Profile Page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,17 +1,54 @@
-import React from "react";
+import React, { useContext } from "react";
 import useLogout from "../hooks/useLogout";
 import useFetchUserData from "../hooks/useFetchUserData";
+import Header from "../common/Header";
+import { MdOutlineArrowBackIosNew } from "react-icons/md";
+import styles from "./Profile.module.css";
+import { FaRegUserCircle } from "react-icons/fa";
+import { FaUserCircle } from "react-icons/fa";
+import { DarkModeContext } from "../context/darkmodeContext";
 
 function Profile() {
   const userDetails = useFetchUserData();
   const logout = useLogout();
 
+  const { darkMode } = useContext(DarkModeContext);
+
   return (
     <div>
+      <div className={styles.headerContainer}>
+        <MdOutlineArrowBackIosNew
+          className={styles.backIcon}
+          onClick={() => window.history.back()}
+        />
+        <Header />
+      </div>
       {userDetails ? (
-        <div>
-          <p>{userDetails.email}</p>
-          <button onClick={logout}>Logout</button>
+        <div className={styles.userDetailsContainer}>
+          <div className={styles.userImageContainer}>
+            {darkMode ? (
+              <FaUserCircle className={styles.userImage} />
+            ) : (
+              <FaRegUserCircle className={styles.userImage} />
+            )}
+          </div>
+          <div>
+            <p className={styles.userDetailsHeader}>First name</p>
+            <p className={styles.userDetails}>{userDetails.fname}</p>
+          </div>
+          <div>
+            <p className={styles.userDetailsHeader}>Last name</p>
+            <p className={styles.userDetails}>{userDetails.lname}</p>
+          </div>
+          <div>
+            <p className={styles.userDetailsHeader}>Email</p>
+            <p className={styles.userDetails}>{userDetails.email}</p>
+          </div>
+          <div className={styles.logoutContainer}>
+            <button onClick={logout} className={styles.logoutButton}>
+              LOGOUT
+            </button>
+          </div>
         </div>
       ) : (
         <div>Loading...</div>
