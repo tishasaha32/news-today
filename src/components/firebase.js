f31536f777298785c07a61ed9d812322f4e787de
Tisha Saha <tishasaha32@gmail.com>
2024-06-06 18:22:24 +0530

    Reset Password Feature done

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -15,5 +15,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-export const auth = getAuth();
+export const auth = getAuth(app);
+
 export default app;
--- src/pages/ResetPassword.js
+++ src/pages/ResetPassword.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ResetPassword.module.css";
 import { Link } from "react-router-dom";
+import { auth } from "../components/firebase";
+import { sendPasswordResetEmail } from "firebase/auth";
 
 function ResetPassword() {
   const logo =
     "https://firebasestorage.googleapis.com/v0/b/news-22cea.appspot.com/o/logo%2FNews%20Today%20Logo.png?alt=media&token=d79b5373-045a-4d25-bbad-04c95ee3157f";
+
+  const [email, setEmail] = useState("");
+  const handleResetPassword = async (e) => {
+    e.preventDefault();
+
+    try {
+      await sendPasswordResetEmail(auth, email);
+      alert("Password reset email sent. Check your inbox.");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className={styles.resetPasswordPageContainer}>
@@ -12,15 +26,20 @@ function ResetPassword() {
         <img src={logo} alt="logo" className={styles.logo} />
       </div>
 
-      <div className={styles.resetPasswordContainer}>
+      <form
+        className={styles.resetPasswordContainer}
+        onSubmit={handleResetPassword}
+      >
         <input
           type="text"
           placeholder="Enter Email"
           className={styles.input}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           required
         />
         <button className={styles.sendResetLinkButton}>SEND RESET LINK</button>
-      </div>
+      </form>
 
       <Link to="/login" style={{ textDecoration: "none", color: "inherit" }}>
         <p className={styles.goBack}>GO BACK</p>
