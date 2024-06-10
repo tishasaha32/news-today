a79b6498a8de88bfc9040bf1460db4fbf2ca3c53
Tisha Saha <tishasaha32@gmail.com>
2024-06-10 15:52:44 +0530

    On component unmount or reload, the speech didnt stops, fixed it

--- src/hooks/useHandleSpeak.js
+++ src/hooks/useHandleSpeak.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function useHandleSpeak({ text }) {
   const handleSpeak = () => {
     if ("speechSynthesis" in window) {
+      window.speechSynthesis.cancel();
+
       const speech = new SpeechSynthesisUtterance(text);
       speech.lang = "en-US";
       window.speechSynthesis.speak(speech);
@@ -10,6 +12,15 @@ function useHandleSpeak({ text }) {
       alert("Sorry, your browser does not support text-to-speech.");
     }
   };
+
+  useEffect(() => {
+    return () => {
+      if ("speechSynthesis" in window) {
+        window.speechSynthesis.cancel();
+      }
+    };
+  }, []);
+
   return { handleSpeak };
 }
 
